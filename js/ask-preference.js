20240ddee99bbb03d0813795219ab2222b198ecf
Cordell Andrade <cordellandrade93@gmail.com>
2020-06-28 23:53:49 -0500

    Fixed data

--- js/ask-preference.js
+++ js/ask-preference.js
@@ -9,8 +9,14 @@ if(response === 'small'){
     message = 'Medium dog full spa priced at $45';
 } else if (response === 'large'){
     message = 'Large dog full spa priced at $65';
-} else {
-    message = 'What size is your dog?';
+}   else if (response === 'Small'){
+        message = 'Small dog full spa priced at $25';
+    } else if (response === 'Medium'){
+        message = 'Medium dog full spa priced at $45';
+    } else if (response === 'Large'){
+        message = 'Large dog full spa priced at $65';
+    } else {
+    message = 'Please refresh page and input size of yor dog';
 }
 document.write('<h3>' + message + '</h3>')
 
